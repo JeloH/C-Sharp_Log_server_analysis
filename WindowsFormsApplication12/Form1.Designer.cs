0a913d0cefa417ba2f1d2ca5f6bcf60e56eadd8c
JeloH <hajelovdar@yahoo.com>
2018-04-14 15:33:15 +0800

    Add files via upload

---
+++ WindowsFormsApplication12/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "View Log File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 206);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Detect Sqlinjection / XSS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 269);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(499, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(785, 28);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(151, 209);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(785, -1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Result";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(9, 412);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(499, 134);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Detection Sql injection : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Detection XSS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(456, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 552);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication12/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+ 
+using System.Data.OleDb;
+using System.Configuration;
+
+using System.Text.RegularExpressions;
+
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace WindowsFormsApplication12
+{
+    public partial class Form1 : Form
+    {
+
+
+        DataSet ds;
+        DataView dv;
+        OleDbDataAdapter da;
+
+        string cmdSql;
+
+        static int sql_injection = 0;
+        static int Xss = 0;
+
+
+        string s3 = @"provider=microsoft.jet.oledb.4.0;" + @"data source=..\\Debug\\report__9.mdb";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //    str1 = ConfigurationManager.ConnectionStrings["cn1"].ConnectionString;
+            OleDbConnection con = new OleDbConnection(s3);
+            OleDbCommand com = new OleDbCommand("select * from tb2", con);
+
+            ds = new DataSet();
+            da = new OleDbDataAdapter(com);
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = ds.Tables[0].TableName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SelectPersonelPrintCard("select * from tb2");
+        }
+
+
+
+        public void SelectPersonelPrintCard(string strCodeSql)
+        {
+
+
+            try
+            {
+
+                OleDbConnection con = new OleDbConnection(s3);
+    
+                con.Open();
+                //جلوگیری از تکرار کد_ملی
+
+                OleDbDataAdapter da = new OleDbDataAdapter(strCodeSql, con);
+                DataTable dt = new DataTable();
+
+
+
+
+                da.Fill(dt);
+                if (dt.Rows.Count == 0)
+                {
+
+                   // MsgBox.ShowMessage(this.Handle.ToInt32(), "چنین شماره عضویتی وجود ندارد", " توجه ", "تایید", "", "", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+
+                }
+
+                else
+                {
+
+                    if (dt.Rows[0]["field1"].ToString() != "")
+                    {
+
+
+
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                          // MessageBox.Show(  );
+
+
+                            if (checkForSQLInjection(dt.Rows[i]["field1"].ToString()) == "True")
+                            {
+
+                                listBox1.Items.Add(dt.Rows[i]["field1"].ToString());
+
+                                Form1.sql_injection = Form1.sql_injection + 1;
+                            }
+
+
+
+                            if (checkFor_XSS(dt.Rows[i]["field1"].ToString()) == "True")
+                            {
+
+                                listBox2.Items.Add(dt.Rows[i]["field1"].ToString());
+
+                                Form1.sql_injection = Form1.Xss + 1;
+                            }
+                   
+                        
+                        }
+ 
+
+ 
+                    }
+
+
+                    label3.Text = listBox1.Items.Count.ToString();
+                    label4.Text = listBox2.Items.Count.ToString();
+
+
+
+
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+              //  MsgBox.ShowMessage(this.Handle.ToInt32(), ex.Message.ToString(), " خطا ", "تایید", "", "", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+
+            }
+
+        }
+
+
+        public static string checkForSQLInjection(string userInput)
+        {
+
+
+            bool isSQLInjection = false;
+            string[] sqlCheckList =  {"select", "drop", "insert", "delete","or",};
+                                  
+ 
+            string CheckString = userInput.Replace("'", "''");
+            for (int i = 0; i <= sqlCheckList.Length - 1; i++)
+            {
+                if ((CheckString.IndexOf(sqlCheckList[i], StringComparison.OrdinalIgnoreCase) >= 0))
+                {
+                    isSQLInjection = true;
+                }
+            }
+            return Convert.ToString(isSQLInjection);
+        }
+
+  public static string checkFor_XSS(string userInput)
+  {
+    bool isXss = false;
+    string[] XssCheckList = { "<script>" , "<script", "/script>",
+   "document.write","alert(" , "alert",
+   "javascript", "location", "document.location","document.cookie", "function" 
+   ,"windows.open","function",
+   ".location"};
+
+   string CheckString = userInput.Replace("'", "''");
+    for (int i = 0; i <= XssCheckList.Length - 1; i++)
+       {
+         if ((CheckString.IndexOf(XssCheckList[i], StringComparison.OrdinalIgnoreCase) >= 0))
+          {
+            isXss = true;
+          }
+        }
+        return Convert.ToString(isXss);
+      }
+
+ 
+
+ 
+  private void Form1_Load(object sender, EventArgs e)
+  {
+
+
+
+
+
+      
+
+  }
+
+  private void button3_Click(object sender, EventArgs e)
+  {
+        
+  }
+
+
+  private bool checkForXSS1(string value)
+  {
+      return value.IndexOf("s") != -1;
+  }
+
+  public static string XSSProtect(string input)
+  {
+      string returnVal = input ?? "";
+
+      returnVal = Regex.Replace(returnVal, @"\<script(.*?)\>(.*?)\<\/script(.*?)\>", "", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+      returnVal = Regex.Replace(returnVal, @"\<style(.*?)\>(.*?)\<\/style(.*?)\>", "", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+      // Thanks to Rubens Farias (http://stackoverflow.com/users/113794/rubens-farias) for help with this part
+      while (Regex.IsMatch(returnVal, @"(<[\s\S]*?) on.*?\=(['""])[\s\S]*?\2([\s\S]*?>)", RegexOptions.Compiled | RegexOptions.IgnoreCase))
+      {
+          returnVal = Regex.Replace(returnVal, @"(<[\s\S]*?) on.*?\=(['""])[\s\S]*?\2([\s\S]*?>)",
+                          delegate(Match match)
+                          {
+                              return String.Concat(match.Groups[1].Value, match.Groups[3].Value);
+                          }, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+      }
+
+      return returnVal;
+  }
+
+  private bool checkForXSS(string value)
+  {
+      Regex regex = new Regex(@"/((\%3C)|<)[^\n]+((\%3E)|>)/I");
+
+      if (regex.Match(value).Success) return true;
+
+      return false;
+  }
+
+  private void button4_Click(object sender, EventArgs e)
+  {
+      this.Text = listBox1.Items.Count.ToString();
+
+      Series series1 = new Series("Spiral");
+
+      series1.ChartType = SeriesChartType.Column;
+
+      //  series1.Points.AddXY("Normal", "");  
+      series1.Points.AddXY("SqlInjection", Form1.sql_injection.ToString());
+    //  series1.Points.AddXY("Ddos", "2");
+      series1.Points.AddXY("XSS", Form1.Xss.ToString());
+
+      series1.Points[0].Color = System.Drawing.Color.Blue;
+    //  series1.Points[1].Color = System.Drawing.Color.Red;
+      series1.Points[1].Color = System.Drawing.Color.Yellow;
+
+      chart1.Series.Add(series1);
+
+
+
+  }
+
+  
+  private void label1_Click(object sender, EventArgs e)
+  {
+
+  }
+
+
+    }
+}
